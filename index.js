8bd38ebe09a746c3e738fd83d096ebd57c6f2d5a
Dhruv Anand <ananddhruv29@gmail.com>
2024-10-05 00:41:08 +0530

    Include provider in User model in database

--- index.js
+++ index.js
@@ -51,6 +51,8 @@ passport.use(new GoogleStrategy({
                 googleId: profile.id,
                 name: profile.displayName,
                 email: profile.emails[0].value
+                email: profile.emails[0].value,
+                provider: 'google'
             });
             console.log("New user.", newUser);
 
