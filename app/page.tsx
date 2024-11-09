82390a163e05353ab866b040773801d751b23afc
Dhruv Anand <ananddhruv29@gmail.com>
2024-11-09 15:49:36 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
 	return (
 		<>
 			<div className="flex flex-col justify-center items-center h-screen">
 				<h1>Welcome to INSPIRE</h1>
+				<Link href={"/poll"} className="p-6">
+					<Button>Get Started</Button>
+				</Link>
 			</div>
 		</>
 	);
