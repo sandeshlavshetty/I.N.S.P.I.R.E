49391e59c58230ea4823c76fc2b9a0332eb5f0ea
Dhruv Anand <ananddhruv29@gmail.com>
2024-11-11 09:15:52 +0530

    Updated `Poll` model

--- models/poll.ts
+++ models/poll.ts
@@ -1,24 +1,17 @@
-import {Schema, model} from 'mongoose';
+import mongoose, { Schema, model, models } from "mongoose";
 
-const voteSchema = new Schema({
-    p_describe : String,
-    date:{
-        type: Date,
-        default: Date.now
-    },
-    visibility : String,
-    option1_name : String,
-    option2_name : String,
-    option3_name : String,
-    option4_name : String,
-    option5_name : String,
-    option1 : Number,
-    option2 : Number,
-    option3 : Number,
-    option4 : Number,
-    option5 : Number
+const pollSchema = new Schema({
+	name: { type: String, required: true },
+	timings: { type: [String], required: true },
+	date: { type: Date, required: true },
+	status: {
+		type: String,
+		enum: ["pending", "active", "ended"],
+		default: "pending",
+	},
+	createdAt: { type: Date, default: Date.now },
 });
 
-const Poll = model('Poll', voteSchema);
+const Poll = models.Poll || model("Poll", pollSchema);
 
 export default Poll;
