c7f071ed996986569495e55027ed33f6f6613f05
Dhruv Anand <ananddhruv29@gmail.com>
2024-11-11 10:10:04 +0530

    Update utils.ts
    Implemented `generateTimeOptions` function to generate the time.

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export const generateTimeOptions = () => {
+	const options = [];
+	for (let hour = 0; hour < 24; hour++) {
+		for (let minute = 0; minute < 60; minute += 30) {
+			options.push(
+				`${hour.toString().padStart(2, "0")}:${minute
+					.toString()
+					.padStart(2, "0")}`
+			);
+		}
+	}
+	return options;
+};
